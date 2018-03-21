bf1f828d9c2f89bcf9fcc43916b4ee6ddc39dde2
Rashmith <salgaonkaar.rashmith.com>
2018-03-21 18:52:28 +0530

    add and remove projects

--- src/App.js
+++ src/App.js
@@ -1,18 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import uuid from 'uuid';
+import $ from 'jquery';
+import Projects from './Components/Projects'
+import AddProject from './Components/AddProject'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      projects: [],
+      todos: []
+    }
+  }
+
+  getToDos() {
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/todos',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({todos: data}, function() {
+          console.log(this.state);
+        })
+      }.bind(this),
+      error: function(err) {
+        console.log(err)
+      }
+    })
+  }
+
+  getProjects() {
+    this.setState({projects: [
+      {
+        id: uuid.v4(),
+        title: "Business Website",
+        category: "Web Design"
+      },
+      {
+        id: uuid.v4(),
+        title: "Social",
+        category: "Mobile Design"
+      },
+      {
+        id: uuid.v4(),
+        title: "Ecommerce Website",
+        category: "Web Development"
+      }
+    ]});
+  }
+
+  componentWillMount() {
+    this.getProjects();
+    this.getToDos();
+  }
+
+  componentDidMount() {
+    this.getToDos();
+  }
+
+  handleAddProject(project) {
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState(projects: projects);
+  }
+
+  handleDelete(id) {
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState(projects: projects);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDelete.bind(this)}/>
       </div>
     );
   }
